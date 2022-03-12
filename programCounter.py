a621de62c6153466b07a8a7def7cd03b05be9cac
ChaozhuQiang <qiangchaozhu@gwu.edu>
2022-03-11 21:25:21 -0500

    aaa

--- programCounter.py
+++ programCounter.py
@@ -1,9 +1,12 @@
-from register import Register
-
-
-class ProgramCounter(Register):
+class ProgramCounter:
     def __init__(self, init):
         self.__value = init
 
     def pc_plus_one(self):
         self.__value = self.__value + 1
+
+    def getValue(self):
+        return self.__value
+
+    def setValue(self, value: int):
+        self.__value = value
